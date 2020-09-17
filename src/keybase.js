9f5e2b38ecdaafd065ed741aab11ac487ba8362c
Michael FIG <mfig@agoric.com>
2020-09-17 13:30:50 -0600

    feat: implement the Keybase bot

---
+++ src/keybase.js
@@ -0,0 +1,219 @@
+import KeybaseBot from 'keybase-bot';
+
+const TRIGGER_COMMAND = '!faucet2';
+
+const ADMIN_CHANNEL = {
+  name: 'agoric.testnet.admins',
+  public: false,
+  topicType: 'chat',
+};
+
+export const runKeybaseBot = async ({ enact, validate, storage, commit }) => {
+  console.log('Initializing Keybase bot...');
+  const bot = new KeybaseBot();
+  await bot.initFromRunningService();
+  console.log('Keybase bot initialized');
+
+  const handleFaucetCommand = async (requestMsg, args) => {
+    // console.log('checking', args);
+    const request = { ...requestMsg, args };
+    const help = await validate(request, TRIGGER_COMMAND).catch(async e => {
+      await bot.chat.send(
+        requestMsg.conversationId,
+        {
+          body: `${e}; try \`${TRIGGER_COMMAND} help\` for more information`,
+        },
+        { replyTo: requestMsg.id },
+      );
+      return false;
+    });
+    if (help === false) {
+      return;
+    }
+    if (typeof help === 'string') {
+      await bot.chat.send(
+        requestMsg.conversationId,
+        {
+          body: `\
+${help}\`\`\`
+${TRIGGER_COMMAND} help
+\`\`\`
+display this help
+`,
+        },
+        { replyTo: requestMsg.id },
+      );
+      return;
+    }
+
+    const thinking = await bot.chat.react(
+      requestMsg.conversationId,
+      requestMsg.id,
+      ':thinking_face:',
+    );
+
+    const body = `\
+@here @${requestMsg.sender.username} wants to \`${args.join(' ')}\`.
+React with :+1: or :-1:`;
+    const question = await bot.chat.send(ADMIN_CHANNEL, { body });
+    // console.log('sent out', approval);
+
+    storage.set(
+      `pending/${question.id}`,
+      JSON.stringify({
+        request,
+        msg: requestMsg,
+        thinking,
+        question: { conversationId: ADMIN_CHANNEL, ...question },
+      }),
+    );
+    commit();
+  };
+
+  const onMessage = async msg => {
+    // console.log('have message', msg);
+    switch (msg.content.type) {
+      case 'reaction': {
+        // console.log('handling reaction', msg);
+        if (msg.channel.name !== ADMIN_CHANNEL.name) {
+          return;
+        }
+        const approveMsg = msg;
+        let approve;
+        if (approveMsg.content.reaction.b === ':-1:') {
+          approve = false;
+        } else if (approveMsg.content.reaction.b === ':+1:') {
+          approve = true;
+        } else {
+          return;
+        }
+        // Look up the pending approval from the reacted-to message.
+        const pendingKey = `pending/${approveMsg.content.reaction.m}`;
+        const pending = storage.get(pendingKey);
+        if (!pending) {
+          return;
+        }
+        // Dig out the request and the original request message.
+        const { request, msg: requestMsg, thinking, question } = JSON.parse(
+          pending,
+        );
+
+        // Mark as no longer pending.
+        storage.delete(pendingKey);
+        commit();
+
+        // Tell them the reaction.
+        // console.log('telling', approveMsg.content.reaction.b);
+        await bot.chat.react(
+          requestMsg.conversationId,
+          requestMsg.id,
+          approveMsg.content.reaction.b,
+        );
+
+        await bot.chat
+          .delete(requestMsg.conversationId, thinking.id)
+          .catch(_ => {});
+        if (!approve) {
+          // Denied, so don't do anything.
+          return;
+        }
+
+        // Try to enact the approval.
+        const hourglass = await bot.chat.react(
+          requestMsg.conversationId,
+          requestMsg.id,
+          ':hourglass_flowing_sand:',
+        );
+        await enact(request, TRIGGER_COMMAND)
+          .then(
+            async body => {
+              await bot.chat.react(
+                requestMsg.conversationId,
+                requestMsg.id,
+                ':white_check_mark:',
+              );
+              if (!body) {
+                return;
+              }
+              await bot.chat.send(
+                question.conversationId,
+                {
+                  body,
+                },
+                { replyTo: question.id },
+              );
+              await bot.chat.send(
+                requestMsg.conversationId,
+                {
+                  body,
+                },
+                { replyTo: requestMsg.id },
+              );
+            },
+            async e => {
+              await bot.chat.send(
+                question.conversationId,
+                {
+                  body: `\
+Failed!
+\`\`\`
+${(e && e.stack) || e}
+\`\`\``,
+                },
+                { replyTo: question.id },
+              );
+              await bot.chat.react(
+                requestMsg.conversationId,
+                requestMsg.id,
+                ':skull_and_crossbones:',
+              );
+            },
+          )
+          .finally(() => {
+            return bot.chat.delete(requestMsg.conversationId, hourglass.id);
+          });
+        break;
+      }
+      case 'text': {
+        const args = msg.content.text.body.trim().split(/\s+/);
+        if (args[0] === TRIGGER_COMMAND) {
+          // console.log('handling', msg);
+          await handleFaucetCommand(msg, args);
+        }
+        break;
+      }
+      default:
+      // console.log('have other message', msg);
+    }
+  };
+
+  // Start watching for new messages.
+  const watchingP = bot.chat.watchAllChannelsForNewMessages(
+    onMessage,
+    undefined,
+    { showLocal: true },
+  );
+
+  const readNextPage = async (channel, pageNum = undefined) => {
+    const res = await bot.chat.read(channel.id, {
+      peek: true,
+      unreadOnly: true,
+      pagination: {
+        next: pageNum,
+      },
+    });
+    for (const msg of res.messages) {
+      // eslint-disable-next-line no-await-in-loop
+      await onMessage(msg);
+    }
+    if (!res.pagination.last) {
+      await readNextPage(channel, res.pagination.next);
+    }
+  };
+
+  // Find and process all our unread messages.
+  const unreadChannels = await bot.chat.list({ unreadOnly: true });
+  await Promise.all(unreadChannels.map(channel => readNextPage(channel)));
+
+  await watchingP;
+};
--- src/main.js
+++ src/main.js
@@ -1,1 +1,82 @@
-export default argv => console.log('boo!', argv);
+import { spawn } from 'child_process';
+
+import { openSwingStore } from '@agoric/swing-store-simple';
+import { runKeybaseBot } from './keybase';
+
+const NETWORK_NAME = 'testnet';
+const AG_SETUP_COSMOS = `${process.env.HOME}/ag-setup-cosmos`;
+
+const q = obj => JSON.stringify(obj, null, 2);
+
+const makeValidate = canProvision => async (request, TRIGGER_COMMAND) => {
+  const [_trigger, cmd, address] = request.args;
+  switch (cmd) {
+    case 'add-egress':
+    case 'provision': {
+      if (!address) {
+        throw Error(`you need to provide an \`agoric1...\` address`);
+      }
+      if (!/^agoric1[a-z0-9]*$/.test(address)) {
+        throw Error(`the address ${q(address)} must be \`agoric1...\``);
+      }
+      if (cmd === 'provision' && !canProvision) {
+        throw Error(`${q(cmd)} not implemented`);
+      }
+      if (!request.sender.username) {
+        throw Error(`need a sending username`);
+      }
+      break;
+    }
+    case 'help': {
+      return `\
+\`\`\`
+${TRIGGER_COMMAND} provision <AGORIC-BECH32>
+\`\`\`
+give delegation tokens to the specified address (something like \`agoric1wa9di7...\`)`;
+    }
+    case undefined: {
+      throw Error(`\`${TRIGGER_COMMAND}\` needs an command`);
+    }
+    default: {
+      throw Error(`${q(cmd)} not implemented`);
+    }
+  }
+  return true;
+};
+
+const makeEnact = validate => async (request, TRIGGER_COMMAND) => {
+  console.log('enacting', request);
+  await validate(request, TRIGGER_COMMAND);
+  return new Promise((resolve, reject) => {
+    const [_trigger, cmd, address] = request.args;
+    switch (cmd) {
+      case 'add-egress':
+      case 'provision': {
+        const command = [
+          AG_SETUP_COSMOS,
+          'shell',
+          `${NETWORK_NAME}/faucet-helper.sh`,
+          cmd,
+          request.sender.username,
+          address,
+        ];
+        console.log('spawn', command, request);
+        const cp = spawn(command[0], command.slice(1));
+        cp.on('exit', resolve);
+        cp.on('error', reject);
+        break;
+      }
+      default:
+        throw Error(`${cmd} not implemented`);
+    }
+  });
+};
+
+export default async _argv => {
+  const { storage, commit } = openSwingStore(`${__dirname}/../db`);
+
+  const validate = makeValidate(true);
+  const enact = makeEnact(validate);
+
+  return Promise.all([runKeybaseBot({ enact, validate, storage, commit })]);
+};
